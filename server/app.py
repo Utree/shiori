0364be31ce9efe46dd8e1f3b2ef85d905b6c0191
Utree <yuukis322+github@gmail.com>
2020-10-29 17:55:55 +0900

    add new api

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, File, UploadFile
 from pydantic import BaseModel
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing import List
+import shutil
+import pathlib
+from datetime import datetime as dt
+from starlette.responses import FileResponse
+from os.path import isfile
 
 app = FastAPI()
 
@@ -41,10 +46,20 @@ class SpotItem(BaseModel):
     thumbnail_url: str
 
 
+class ContentItem(BaseModel):
+    id: int
+    spot_id: int
+    file_url: str
+
+
 class SpotInfo(BaseModel):
     """ スポット情報返却項目"""
 
     data: List[SpotItem]
+
+
+class ContentInfo(BaseModel):
+    data: List[ContentItem]
 
 
 class RecomendItem(BaseModel):
@@ -97,12 +112,12 @@ def get_current_user(
         print(cred.credentials)
         # TODO: tokenを使ってdbにuser名を参照し認証
         user = SignupInfo(name="user1", email="a@a.com", password="aaa")
-    except:
+    except Exception:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Invalid authentication credentials',
             headers={'WWW-Authenticate': 'Bearer'},
-        )
+            )
     return user
 
 
@@ -113,11 +128,16 @@ async def get_travel_list(current_user=Depends(get_current_user)):
     # TODO: DBからユーザーが持つtravelの一覧を参照
     sample_data = TravelInfo(
         data=[
-            TravelItem(id=0, name="箱根旅行", period="2020/1/1", thumbnail_url="https://picsum.photos/250?image=5"),
-            TravelItem(id=3, name="沖縄旅行", period="2020/1/2", thumbnail_url="https://picsum.photos/250?image=10"),
-            TravelItem(id=10, name="北海道旅行", period="2020/1/3", thumbnail_url="https://picsum.photos/250?image=11"),
-            TravelItem(id=15, name="修学旅行", period="2020/1/4", thumbnail_url="https://picsum.photos/250?image=20"),
-            TravelItem(id=80, name="パリ", period="2020/1/5", thumbnail_url="https://picsum.photos/250?image=50")
+            TravelItem(id=0, name="箱根旅行", period="2020/1/1",
+                       thumbnail_url="https://picsum.photos/250?image=5"),
+            TravelItem(id=3, name="沖縄旅行", period="2020/1/2",
+                       thumbnail_url="https://picsum.photos/250?image=10"),
+            TravelItem(id=10, name="北海道旅行", period="2020/1/3",
+                       thumbnail_url="https://picsum.photos/250?image=11"),
+            TravelItem(id=15, name="修学旅行", period="2020/1/4",
+                       thumbnail_url="https://picsum.photos/250?image=20"),
+            TravelItem(id=80, name="パリ", period="2020/1/5",
+                       thumbnail_url="https://picsum.photos/250?image=50")
             ])
 
     return sample_data
@@ -125,26 +145,81 @@ async def get_travel_list(current_user=Depends(get_current_user)):
 
 @app.get("/recomends", response_model=RecomendInfo)
 async def get_recomend_list(latitude: float, longitude: float):
-    """ レコメンドを返す
+    """ recomendsを返す
     """
     # TODO: DBからユーザーが持つtravelの一覧を参照
     sample_data = RecomendInfo(
         data=[
-            RecomendItem(name="タワー", thumbnail_url="https://picsum.photos/250?image=5"),
-            RecomendItem(name="遊園地", thumbnail_url="https://picsum.photos/250?image=10"),
-            RecomendItem(name="動物園", thumbnail_url="https://picsum.photos/250?image=11"),
-            RecomendItem(name="ショッピングモール", thumbnail_url="https://picsum.photos/250?image=20"),
-            RecomendItem(name="この世", thumbnail_url="https://picsum.photos/250?image=50")
+            RecomendItem(name="タワー",
+                         thumbnail_url="https://picsum.photos/250?image=5"),
+            RecomendItem(name="遊園地",
+                         thumbnail_url="https://picsum.photos/250?image=10"),
+            RecomendItem(name="動物園",
+                         thumbnail_url="https://picsum.photos/250?image=11"),
+            RecomendItem(name="ショッピングモール",
+                         thumbnail_url="https://picsum.photos/250?image=20"),
+            RecomendItem(name="この世",
+                         thumbnail_url="https://picsum.photos/250?image=50")
             ])
 
     return sample_data
+
+
+@app.get("/spots", response_model=SpotInfo)
+async def get_spot_list(travel_id: int):
+    """ spotsを返す
+    """
+    # TODO: DBからspotの一覧を参照
+    sample_data = SpotInfo(
+        data=[
+            SpotItem(
+                id=1, name="aaa", created_at="2020/1/1",
+                latitude=1.111, longitude=1.111,
+                thumbnail_url="https://picsum.photos/250?image=50"
+                ),
+            SpotItem(
+                id=1, name="bbb", created_at="2020/1/1",
+                latitude=1.111, longitude=1.111,
+                thumbnail_url="https://picsum.photos/250?image=50"
+                )
+            ])
+
+    return sample_data
+
+
+@app.get("/contents", response_model=ContentInfo)
+async def get_content_list(travel_id: int):
+    """ contentsを返す
+    """
+    # TODO: DBからcontentの一覧を参照
+    sample_data = ContentInfo(
+        data=[
+            ContentItem(
+                id=1, spot_id=1,
+                file_url="https://picsum.photos/250?image=50"
+                ),
+            ContentItem(
+                id=1, spot_id=1,
+                file_url="https://picsum.photos/250?image=50"
+                ),
+            ])
+
+    return sample_data
+
+
+@app.get("/content")
+async def get_content(name: str):
+    " contentを返す"
+    path = f"uploaded_file/{name}"
+    if isfile(path):
+        return FileResponse(path)
 
 
 @app.post("/add_travel")
 async def add_travel(travel: TravelItem):
     """ 旅行のアルバムを作成する
     """
-    # TODO: DBにユーザーが加えたいアルバムを追加
+    # TODO: DBにアルバムを追加
 
     name = travel.name
     period = travel.period
@@ -154,12 +229,39 @@ async def add_travel(travel: TravelItem):
 
 @app.post("/add_spot")
 async def add_spot(spot: SpotItem):
-    """ 旅行のアルバムに
+    """ 旅行のアルバムに紐付けられたスポットを追加する
     """
-    # TODO: DBにユーザーが加えたいアルバムを追加
+    # TODO: DBにアルバムを追加
 
     name = spot.name
-    latitude = spot.float
-    longitude = spot.float
+    # latitude = spot.float
+    # longitude = spot.float
 
     print(f"DEBUG[add_travel]\tname: {name}")
+
+
+@app.post("/add_content")
+async def add_content(content: ContentItem):
+    """ スポットに紐付けられたコンテンツを追加する
+    """
+    # TODO: DBにアルバムを追加
+    file_url = content.file_url
+
+    print(f"DEBUG[add_content]\tfile_url: {file_url}")
+
+
+@app.post("/uploadfile")
+async def create_upload_file(file: UploadFile = File(default=None)):
+    """ ファイルをアップロードする
+    """
+    # timestampを生成
+    tdatetime = dt.now()
+    tstr = tdatetime.strftime('%Y-%m-%d_%H-%M-%S')
+    # renameしてファイルを保存
+    destination = pathlib.Path(f"uploaded_file/{tstr}_{file.filename}")
+    try:
+        with destination.open("wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+    finally:
+        file.file.close()
+    return {"filename": f"uploaded_file/{tstr}_{file.filename}"}
